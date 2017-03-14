70f9c9826b015f0acb5ec1d8152800aa55fb3953
T.Priyanthan <Thuraisingam.priyanthan@auxenta.com>
2017-03-14 14:24:32 +0530

    angular added

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
-var app = require('express')();
-var http = require('http').Server(app);
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'Angular/angular-sample/dist')));
+
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello World</h1>')
+ //res.send('<h1>Hello World</h1>')
+  res.sendFile(path.join(__dirname, 'Angular/angular-sample/dist'));
 });
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on *:3000');
-});
